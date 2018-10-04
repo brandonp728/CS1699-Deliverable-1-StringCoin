23ab64c2cd86764f0954ae7aca4b000b50b652d2
Brandon  Palonis <bmp59@pitt.edu>
2018-10-04 02:06:41 -0400

    Add files via upload

--- StringCoin.java
+++ StringCoin.java
@@ -117,10 +117,7 @@ public class StringCoin
       }
 
       if(verified) {
-        System.out.println("Adding " + coin);
         coinKeyMap.replace(coin, coinKeyMap.get(coin), pk);
-      } else {
-        System.out.println("Missed " + coin);
       }
 
 
